be6d22a3f1e57d4c30bc1ee899a63ad6e86f439d
LamaAlangari <142490239+LamaAlangari@users.noreply.github.com>
2024-05-08 00:35:57 +0300

    Update editRequest.php

--- editRequest.php
+++ editRequest.php
@@ -69,7 +69,7 @@ mysqli_close($conn);
                 <li><a href="viewprofileLearner.html">EDIT PROFILE</a></li>
                 <br>
 
-                <li><a href="#">SIGN OUT</a></li>
+                <li><a href="Signout.php">SIGN OUT</a></li>
             </ul></li>
 
         </div></nav>
