3af4d7bb46947e731d3f9f38b8cdd7beb9982535
danaksu <danasaad037@gmail.com>
2024-05-08 11:18:21 +0300

    .

--- currentSessionLearner.php
+++ currentSessionLearner.php
@@ -26,11 +26,10 @@ $stmt->close();
 <head>
     <meta charset="utf-8" />
     <title>Olang</title>
-    
+    <link rel="stylesheet" href="navbar.css" />
     <link rel="stylesheet" href="home view profile.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-   
-    <link rel="stylesheet" href="navbar.css" />
+
     <link rel="stylesheet" href="sessionsDesignPreviousAndCurrent.css" />
     
 </head>
